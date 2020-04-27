ee0b6025b0198089267fef6f68376120c5a7dc24
chp851125 <62477512+chp851125@users.noreply.github.com>
2020-04-27 17:07:58 +0800

    20200427
    將結果儲存陣列中，以利維護

--- IT_ironman.py
+++ IT_ironman.py
@@ -1,13 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
 
 def get_info(articles):
     for article in articles:
+        array = [
+            article.select('div.qa-list__info a.qa-list__info-time')[0].text,
+            article.select('div.qa-list__series a')[0].text,
+            article.select('h3.qa-list__title a')[0].text,
+            article.select('h3.qa-list__title a')[0]['href']
+        ]
         try:
-            print(article.select('div.qa-list__info a.qa-list__info-time')[0].text, article.select('div.qa-list__series a')[0].text, "：", article.select('h3.qa-list__title a')[0].text, article.select('h3.qa-list__title a')[0]['href'])
+            print(array[0], array[1], ":", array[2], array[3])
         except Exception as err:
-            print(article.select('div.qa-list__info a.qa-list__info-time')[0].text, article.select('h3.qa-list__title a')[0].text, article.select('h3.qa-list__title a')[0]['href'])
+            print(array[0], array[2], array[3])
+'''
+def excel():
+    # 將數據寫入Excel 
+    wb = Workbook() 
+    # 設置Excel文件名 
+    dest_filename = 'UserInfoFile.xlsx' 
+    # 新建一個表 
+    ws1 = wb.active 
+    # 設置表頭 
+    titleList = ['時間', '網址', '招聘企業', '學校', '地址'] for row in range(len(titleList)): c = row + 1 ws1.cell(row=1, column=c, value=titleList[row]) 
+    # 填寫表內容 
+    for listIndex in range(len(setSQLData)): 
+        ws1.append(setSQLData[listIndex]) 
+    wb.save(filename=dest_filename)
+'''
 
 def crawler():
     url = "https://ithelp.ithome.com.tw/tags/articles/%E9%90%B5%E4%BA%BA%E8%B3%BD?tab=hot"
@@ -19,5 +41,5 @@ def crawler():
         next_url = paging[len(paging) - 1]['href']  # 每頁paging陣列個數可能不同，但一定是最後一個
         url = next_url
         get_info(articles)
-        
+
 crawler()
